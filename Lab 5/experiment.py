3002271b59d35e49d7dde930dbe682a18fe71910
Ranoshis Das <127428253+RanoshisDas@users.noreply.github.com>
2025-03-10 09:01:56 +0000

    yhy

--- Lab 5/experiment.py
+++ Lab 5/experiment.py
@@ -6,13 +6,13 @@ import pandas as pd, numpy as np
 
 # Importing all datasets
 churn_data = pd.read_csv("churn_data.csv")
-churn_data.head()
+print(churn_data.head())
 
 customer_data = pd.read_csv("customer_data.csv")
-customer_data.head()
+print(customer_data.head())
 
 internet_data = pd.read_csv("internet_data.csv")
-internet_data.head()
+print(internet_data.head())
 
 
 # df_1 = pd.merge(churn_data, customer_data, how='inner', on='customerID')
